4fe10f6f5757b95f58f81270bcae31c087c77b9a
Maciej Jadach <maciej.jadach@grandparade.co.uk>
2021-08-26 19:16:26 +0200

    remove port from client

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -20,7 +20,7 @@ const App = () => {
     useEffect(() => {
         const setUpClient = async () => {
             if (!client) {
-                const client = await new Colyseus.Client('wss://' + HOST + `:${PORT}`);
+                const client = await new Colyseus.Client('wss://' + HOST);
                 setClient(client);
             }
         }
