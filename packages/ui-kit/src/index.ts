8d1e0284741891a617d395603a6b1de155dbd5e3
mjdch <30183876+mjdch@users.noreply.github.com>
2023-02-03 18:44:05 +0100

    fix(ui-kit): Trigger exports

--- packages/ui-kit/src/index.ts
+++ packages/ui-kit/src/index.ts
@@ -4,7 +4,7 @@ export { Decision } from './components/decisions';
 export { PlayerList } from './components/player-list';
 export { Spinner } from './components/spinner';
 export { Logo } from './components/logo';
-export { Modal } from './components/modal';
 export { Alert } from './components/alert';
 export { Button } from './components/button';
 export { Badge } from './components/badge';
+export { Modal } from './components/modal';
